96b49c1bcaad6991d45c4afa87714d175df500e8
geumyoung <1gyn093@gmail.com>
2024-02-13 23:36:48 +0900

    prisma findMany

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import Image from 'next/image';
+import { db } from '@/db';
+import { PrismaClient } from '@prisma/client';
 
 export default function Home() {
+	async function getSnippet() {
+		const snippets = await db.snippet.findMany();
+		console.log('snippets_?', snippets);
+	}
+	getSnippet();
+
 	return <main></main>;
 }
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,5 +1,4 @@
 import { db } from '@/db';
-import { redirect } from 'next/dist/server/api-utils';
 
 export default function NewSnippet() {
 	async function submitHandler(formData: FormData) {
