c2faefd5d25d82373184c2218cf55582828ea035
geumyoung <1gyn093@gmail.com>
2024-02-16 01:05:05 +0900

    snippets 목록으로 보여주기, snippet 상세 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
 import { db } from '@/db';
 import { PrismaClient } from '@prisma/client';
+import Link from 'next/link';
 
-export default function Home() {
-	async function getSnippet() {
-		const snippets = await db.snippet.findMany();
-		console.log('snippets_?', snippets);
-	}
-	getSnippet();
+export default async function Home() {
+	const snippets = await db.snippet.findMany();
 
-	return <main></main>;
+	return (
+		<main>
+			<div>
+				<div className='flex m-2 justify-between items-center'>
+					<h1 className='text-xl font-bold'>스니펫 목록</h1>
+					<Link href={'/snippets/new'} className='border p-2 border-rounded'>
+						만들기
+					</Link>
+				</div>
+				<div className='flex flex-col gap-2'>
+					{snippets.map(item => {
+						return (
+							<Link
+								href={`/snippets/${item.id}`}
+								className='flex justify-between items-center p-2 border rounded'
+								key={item.id}
+							>
+								<div>{item.title}</div>
+								<div>보기</div>
+							</Link>
+						);
+					})}
+				</div>
+			</div>
+		</main>
+	);
 }
---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+
+export default async function Detail({ params }: { params: { id: string } }) {
+	console.log(typeof params.id);
+	const snippetId = parseInt(params.id);
+	const snippet = await db.snippet.findFirst({ where: { id: snippetId } });
+	if (!snippet) {
+		return notFound();
+	}
+	return (
+		<div>
+			<div className='flex m-4 justify-between items-center'>
+				<h1 className='text-xl font-bold'>{snippet.title}</h1>
+				<div className='flex gap-4'>
+					<button className='p-2 border rounded'>편집</button>
+					<button className='p-2 border rounded'>삭제</button>
+				</div>
+			</div>
+			<pre className='p-3 border rounded bg-gray-200 border-gray-200'>
+				<code>{snippet.code}</code>
+			</pre>
+		</div>
+	);
+}
