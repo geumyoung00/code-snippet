14a89279030487fa5c8df9e00e289c13b9afd84a
geumyoung <1gyn093@gmail.com>
2024-02-18 23:07:15 +0900

    수정 페이지 작업 중

---
+++ src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,20 @@
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+import EditorPage from '@/components/editor/page';
+
+export default async function EditPage({ params }: { params: { id: string } }) {
+	const snippetId = parseInt(params.id);
+
+	const snippet = await db.snippet.findFirst({ where: { id: snippetId } });
+
+	if (!snippet) {
+		return notFound();
+	}
+
+	return (
+		<div>
+			<h1>{snippet.title}</h1>
+			<EditorPage snippet={snippet} />
+		</div>
+	);
+}
---
+++ src/components/editor/page.tsx
@@ -0,0 +1,14 @@
+import Editor from '@/components/editor/page';
+
+export default function EditorPage(props: any) {
+	return (
+		<div>
+			{/* <Editor
+				height='90vh'
+				defaultLanguage='javascript'
+				defaultValue='// some comment'
+				onChange={handleEditorChange}
+			/> */}
+		</div>
+	);
+}
